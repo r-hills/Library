2c068b17af28deb3044c3f77598ae3f4136249b7
Ashlin Aronin & Rick Hills <student@epicodus.com>
2015-08-26 12:09:31 -0700

    Write all tests for Patron and make them fail

--- tests/PatronTest.php
+++ tests/PatronTest.php
@@ -37,6 +37,166 @@
             $this->assertEquals($name, $result);
         }
 
+        function test_save()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            //Act
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals($test_patron, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            $name2 = "Ian Browne";
+            $phone2 = "4";
+            $email2 = "b@b.com";
+            $test_patron2 = new Patron($name2, $phone2, $email2);
+            $test_patron2->save();
+
+            //Act
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patron, $test_patron2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            $name2 = "Ian Browne";
+            $phone2 = "4";
+            $email2 = "b@b.com";
+            $test_patron2 = new Patron($name2, $phone2, $email2);
+            $test_patron2->save();
+
+            //Act
+            Patron::deleteAll();
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+        function test_delete()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            $name2 = "Ian Browne";
+            $phone2 = "4";
+            $email2 = "b@b.com";
+            $test_patron2 = new Patron($name2, $phone2, $email2);
+            $test_patron2->save();
+
+            //Act
+            $test_patron->delete();
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patron2], $result);
+        }
+
+        function test_updateName()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            //Act
+            $new_name = "Billy Worthington";
+            $test_patron->updateName($new_name);
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals($new_name, $result[0]->getName());
+        }
+
+        function test_updatePhone()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            //Act
+            $new_phone = "888";
+            $test_patron->updatePhone($new_phone);
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals($new_phone, $result[0]->getPhone());
+        }
+
+        function test_updateEmail()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            //Act
+            $new_email = "hi@bye.com";
+            $test_patron->updateEmail($new_email);
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals($new_email, $result[0]->getEmail());
+        }
+
+        function test_find()
+        {
+            //Arrange
+            $name = "Dan Brown";
+            $phone = "3";
+            $email = "a@a.com";
+            $test_patron = new Patron($name, $phone, $email);
+            $test_patron->save();
+
+            //Act
+            $result = Patron::find($test_patron->getId());
+
+            //Assert
+            $this->assertEquals($test_patron, $result);
+        }
+
+
+
+
+
+
+
 
 
     }
