f7e347e999a0744ebeede438f65c0b261118a458
Ashlin Aronin & Rick Hills <student@epicodus.com>
2015-08-26 10:28:46 -0700

    Add shell functions for Book class.

---
+++ src/Book.php
@@ -0,0 +1,82 @@
+<?php
+
+    class Book
+    {
+        private $title;
+        private $id;
+
+        function __construct($title, $id = null)
+        {
+            $this->title = $title;
+            $this->id = $id;
+        }
+        function setTitle ($new_title)
+        {
+
+        }
+
+        function getTitle ()
+        {
+
+        }
+
+        function getId ()
+        {
+
+        }
+
+        function save ()
+        {
+
+        }
+
+        function delete ()
+        {
+
+        }
+
+        function updateTitle ($new_title)
+        {
+
+        }
+
+        function getAuthors ()
+        {
+
+        }
+
+        function addAuthor ($new_author)
+        {
+
+        }
+
+        function getCopies ()
+        {
+
+        }
+
+        function addCopy ()
+        {
+
+        }
+
+        static function find ($search_id)
+        {
+
+        }
+
+        static function deleteAll ()
+        {
+
+        }
+
+        static function getAll ()
+        {
+
+        }
+
+
+
+    }
+
+?>
---
+++ tests/BookTest.php
@@ -0,0 +1,25 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Book.php";
+    //require_once "src/Author.php";
+    //require_once "src/Patron.php";
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BookTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Book::deleteAll();
+            // Author::deleteAll();
+            // Patron::deleteAll();
+        }
+    }
+?>
